4a6df19df66f6af1b47658c543bda40343743520
Unybox <johnandqueennn@gmail.com>
2024-07-16 21:33:17 +0700

    ImGUi setup

--- lib/src/main/cpp/main.cpp
+++ lib/src/main/cpp/main.cpp
@@ -6,6 +6,15 @@
 #include <cstring>
 #include <thread>
 #include "zygisk.hpp"
+// ImGui
+#include "imgui.h"
+#include "imgui_impl_android.h"
+#include "imgui_impl_opengl3.h"
+// EGL
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+// Dobby
+#include "dobby.h"
 
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
@@ -13,6 +22,7 @@ using zygisk::ServerSpecializeArgs;
 
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "MyModule", __VA_ARGS__)
 
+static int width, height;
 
 // -- ZYGISK
 
@@ -34,7 +44,7 @@ public:
 
     void postAppSpecialize(const AppSpecializeArgs *) override {
         if (createThread) {
-            std::thread hackThread(inject, targetProcessName);
+            std::thread hackThread(inject, gameDataDir);
             LOGD("Thread created");
         }
         
@@ -44,7 +54,7 @@ private:
     Api *api;
     JNIEnv *env;
     bool createThread = false;
-
+    char *gameDataDir;
 
     void preSpecialize(const char *processName, const char *appDataDir) {
         
@@ -52,6 +62,9 @@ private:
         if (strcmp(processName, targetProcessName) == 0) {
             LOGD("Success, setup a thread");
             createThread = true;
+            gameDataDir = new char[strlen(appDataDir) + 1];
+            strcpy(gameDataDir, appDataDir);
+
         } else {
             LOGD("Skip, process unknown");
         }
@@ -64,16 +77,70 @@ REGISTER_ZYGISK_MODULE(MyModule)
 
 // -- end ZYGISK
 
+// Draw ImGui Menu
+using namespace ImGui;
+bool setupImGui;
+bool exampleCheckbox;
+void modMenu() {
+	Begin("Mod Menu");
+	ImGuiTabBarFlags tabBarFlags = ImGuiTabBarFlags_FittingPolicyResizeDown;
+	if (BeginTabBar("Menu", tabBarFlags)) {
+		if (BeginTabItem("Player")) {
+			Checkbox("This is checkbox", &exampleCheckbox);
+			EndTabItem();
+		}
+		EndTabItem();
+	}
+}
+void drawImGui() {
+	IMGUI_CHECKVERSION();
+	CreateContext();
+	ImGuiIO &io = GetIO();
+	io.DisplaySize = ImVec2((float) width, (float) height);
+	ImGui_ImplOpenGL3_Init("#version 300");
+	StyleColorsDark(); // Default
+	GetStyle().ScaleAllSizes(10.0f);
+}
+
 // -- HOOK IMGUI
 
 // INPUT HANDLER
+/* android::InputConsumer::initializeMotionEvent(android::MotionEvent*, android::InputMessage const*)
+ * void InputConsumer::initializeMotionEvent(MotionEvent* event, const InputMessage* msg)
+ */
+void (*inputOrig)(void* thiz, void* event, void* msg);
+void inputHook(void *thiz, void *event, void *msg) {
+    inputOrig(thiz, event, msg);
+    ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz);
+} 
 
 // EGLSWAPBUFFER HANDLER
-
+EGLBoolean (*eglSwapBufferOrig)(EGLDisplay display, EGLSurface surface);
+EGLBoolean eglSwapBufferHook(EGLDisplay display, EGLSurface surface) {
+    eglQuerySurface(display, surface, EGL_WIDTH, &width);
+    eglQuerySurface(display, surface, EGL_HEIGHT, &height);
+	if (!setupImGui) {
+		drawImGui();
+		setupImGui = true;
+	}
+	ImGuiIO &io = GetIO();
+	ImGui_ImplOpenGL3_NewFrame();
+	NewFrame();
+	modMenu();
+	EndFrame();
+	Render();
+	glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    return eglSwapBufferOrig(display, surface);	
+}
 
 // INJECT OUR MENU
 void inject(const char *targetProcessName) {
-    // HOOK SYMBOL
+    // HOOK INPUT SYMBOL
+    DobbySymbolResolver("libinput.so", "_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE");
+
+    // HOOK EGLSWAPBUFFER
+    DobbySymbolResolver("libEGL.so", "eglSwapBuffers");
 }
 
 // -- END HOOK IMGUI
--- lib/src/main/cpp/main.h
+++ lib/src/main/cpp/main.h
@@ -1,7 +1,7 @@
 #ifndef DIRECTIVE_STATEMENT
 #define DIRECTIVE_STATEMENT
 
-void inject(const char *targetProcessName);
+void inject(const char *gameDataDir);
 const char *targetProcessName = "com.unity.test";
 
 #endif
